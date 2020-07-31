c16b52fabda2a0f1be43b20e0d68d61f2ed1f08f
ifedayo <adesiyanifedayo@gmail.com>
2020-07-31 16:52:23 +0100

    Add POST and GET methods

--- rest-api/server.js
+++ rest-api/server.js
@@ -1,28 +1,47 @@
 const express = require('express')
-const logger = require('morgan')
-const errorhandler = require('errorhandler')
-const bodyParser = require('body-parser')
+// const logger = require('morgan')
+// const errorhandler = require('errorhandler')
+// const bodyParser = require('body-parser')
 
+const restaurants = [
+	{id: 1, name:"honeygrill"},
+	{id: 2, name:"chicken republic"},
+	{id: 3, name:"mr biggs"},
+];
 
-let store = {}
-store.accounts = []
+const app = express()
+app.use(express.json())
 
-let app = express()
-app.use(bodyParser.json())
-app.use(logger('dev'))
-app.use(errorhandler)
 
 //GET METHOD
+app.get('/api/restaurants',(req, res)=>{
+	res.send(restaurants)
+})
+
+app.get('/api/restaurants/:id', (req, res)=>{
+	const restaurant = restaurants.find(r => r.id === parseInt(req.params.id))
+	if (!restaurant) res.status(404).send('The restaurant was not found')
+		res.send(restaurant)
+})
+
 
 
 //POST METHOD
-
+app.post('/api/restaurants', (req, res)=>{
+	const restaurant = {
+		id: restaurants.length + 1,
+		name: req.body.name
+	}
+	restaurants.push(restaurant)
+	res.send(restaurant)
+})
 
 //PUT METHOD
 
 //DELETE METHOD
 
 
+//PORT
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000, ()=>{console.log("Listening on port 3000.....")})
+app.listen(port, ()=>{console.log('Listening on port ',port,'.....')})
