b7d0a46b399ad842c43b1a810e488182932f7196
ifedayo <adesiyanifedayo@gmail.com>
2020-08-05 17:18:20 +0100

    Add Error callback

--- mongodb-script-project/mongo.js
+++ mongodb-script-project/mongo.js
@@ -3,3 +3,10 @@ const MongoClient = mongodb.MongoClient
 
 
 const url = 'mongodb://localhost:27017/node-course-db'
+
+
+MongoClient.connect(url, (err, db)=>{
+	if (err) return process.exit(1)
+	console.log('Connected successfully to server')
+	db.close()
+})
